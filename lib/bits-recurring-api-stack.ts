e173ce47f3c4abbc804eda55a94608770cffc614
Abdou Kaba <akabadev@gmail.com>
2025-06-08 23:19:37 +0200

    Transform the source with Models and Controllers

--- lib/bits-recurring-api-stack.ts
+++ lib/bits-recurring-api-stack.ts
@@ -1,16 +1,58 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
 
 export class BitsRecurringApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const table = new dynamodb.Table(this, 'RecurringPayments', {
+      partitionKey: { name: 'paymentId', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BitsRecurringApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const fn = new lambda.Function(this, 'SubmitPaymentFunction', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset('dist/src'),
+      handler: 'lambda/index.handler',
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    table.grantWriteData(fn);
+
+    // Add Cognito User Pool
+    const userPool = new cognito.UserPool(this, 'UserPool', {
+      selfSignUpEnabled: true,
+      signInAliases: { email: true },
+      autoVerify: { email: true }
+    });
+
+    const userPoolClient = new cognito.UserPoolClient(this, 'UserPoolClient', {
+      userPool,
+    authFlows: {
+    userPassword: true,   // <-- enables USER_PASSWORD_AUTH
+    userSrp: true,
+  },
+    });
+
+    const api = new apigateway.RestApi(this, 'RecurringPaymentsApi', {
+      restApiName: 'Recurring Payments Service',
+    });
+
+    const authorizer = new apigateway.CognitoUserPoolsAuthorizer(this, 'CognitoAuthorizer', {
+      cognitoUserPools: [userPool],
+    });
+
+    const payments = api.root.addResource('payments');
+
+    payments.addMethod('POST', new apigateway.LambdaIntegration(fn), {
+      authorizer,
+      authorizationType: apigateway.AuthorizationType.COGNITO,
+    });
   }
 }
